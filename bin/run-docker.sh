7bb6d601010903e7a3563f9bde79a6b90e5d36a0
Ashith Wilson <ashithwilson@gmail.com>
2022-06-22 17:44:22 +0530

    Add docker container name

--- bin/run-docker.sh
+++ bin/run-docker.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 IMAGE=ubuntu-ssh
+CONTAINER_NAME="$IMAGE"
 
-current_docker=$( docker ps | grep "$IMAGE" | awk '{print $1}' )
+# Make sure to remove any existing docker containers
 
-[ -z "$current_docker" ]         \
-|| docker kill "$current_docker"
-
+docker rm -f "$CONTAINER_NAME" 2>/dev/null
 
 docker run -d                                                       \
 -h machine-test                                                     \
 -p 2222:22                                                          \
+--name "$CONTAINER_NAME"                                            \
 --mount type=bind,source="$(pwd)"/logs,target=/var/log/machine-test \
 "$IMAGE"
 
 # Kill ngrok session if it already exists
-ngrok_old_pid=$( ps aux | grep ngrok | grep tcp | grep 2222 | grep -v grep | awk '{print $2}' )
-[ -z "$ngrok_old_pid" ] || kill -9 "$ngrok_old_pid"
+ngrok_current_pid=$( ps aux | grep ngrok | grep tcp | grep 2222 | grep -v grep | awk '{print $2}' )
+[ -z "$ngrok_current_pid" ] || kill -9 "$ngrok_current_pid"
 
 # Start a new ngrok session
 ngrok tcp 2222 
